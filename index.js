f4255212e307b9bdf6891118a610a439163ca9e8
Aviad Sapir <aviadsapir@gmail.com>
2018-09-23 14:06:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+function getFirstSelector(selector){
+ return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.querySelector('#nested.target');
+}
+function increaseRankBy(n){
+  const lis = document.querySelectorAll(".ranked-lists");
+  for (let i=0; i< lis.length; i++){
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) +1).toString();
+  }
+}
+function deepestChild() {
+  var inception = document.getElementById('grand-node');
+  var nolan = inception.children[0];
+  while (nolan) {
+    inception = nolan;
+    nolan = inception.children[0];
+  }
+  return inception;
+} 

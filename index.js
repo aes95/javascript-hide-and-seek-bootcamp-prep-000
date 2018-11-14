262212799a144aa3649b75049b916a10f85e1289
Aviad Sapir <aviadsapir@gmail.com>
2018-11-14 01:54:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ function getFirstSelector(selector){
  return document.querySelector(selector);
 }
 function nestedTarget(){
+<<<<<<< HEAD
   return document.querySelector('#nested.target');
+=======
+  return document.querySelector('#nested .target');
+>>>>>>> 3066bdb615fd473f4643a887d05cd8fb36b82a45
 }
 function increaseRankBy(n){
   const lis = document.querySelectorAll(".ranked-lists");
@@ -11,6 +15,7 @@ function increaseRankBy(n){
     lis[i].innerHTML = (parseInt(lis[i].innerHTML) +1).toString();
   }
 }
+<<<<<<< HEAD
 function deepestChild() {
   var inception = document.getElementById('grand-node');
   var nolan = inception.children[0];
@@ -20,3 +25,8 @@ function deepestChild() {
   }
   return inception;
 } 
+=======
+function deepestChild(){
+  
+}
+>>>>>>> 3066bdb615fd473f4643a887d05cd8fb36b82a45
